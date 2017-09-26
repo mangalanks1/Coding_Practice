0b436513c7410a4a2bb8cf695afdba9d5cc406fa
mangalanks1 <mangalanks@gmail.com>
2017-09-26 17:04:06 -0400

    Merge Sort Algo

--- MergeSort.py
+++ MergeSort.py
@@ -13,8 +13,25 @@ def merge(A,B):
     elif A[i] > B[j]:
       C.append(B[j])
       j=j+1
-  if i == n1-1: C.append(B[j::])
-  else: C.append(A[i::]]
+  if i == n1-1: 
+    C.append(B[j::])
+  else: 
+    C.append(A[i::]]
   return(C)
                  
-                
+def mergesort(x):
+  n = len(x)
+  mid = int(n/2)
+  if n==0 or n==1: return x
+  else:
+    a = mergesort(x[0:mid])
+    b = mergesort(x[mid::])
+    C = merge(a,b)
+    return C
+
+if name == '__main__':
+  x = [1,4,7,4,9,22,66,88,6,66,100]
+  print("Jumbled Array is", x)
+  print(mergesort(x))
+  
+             
